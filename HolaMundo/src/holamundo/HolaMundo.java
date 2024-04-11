ba8d7aabd9c250c9cf9beb1c97a4dd99e5a908d4
hadiminou <94901618+hadiminou@users.noreply.github.com>
2024-04-11 21:47:21 +0200

    Add files via upload

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,11 @@
+package holamundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hola mundo");
+		int a = 7;
+	}
+
+}
